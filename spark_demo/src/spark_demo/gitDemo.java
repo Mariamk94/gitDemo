aac74e09051770f83280009065e5c9b9c999ced0
MariyamA <MariyamA@D12000664.namindia.com>
2017-11-03 12:50:14 +0530

    commit to branch 2

--- spark_demo/src/spark_demo/gitDemo.java
+++ spark_demo/src/spark_demo/gitDemo.java
@@ -4,7 +4,7 @@ public class gitDemo {
 
 	public static void main(String[] args) {
 		System.out.println("git is simple");
-		System.out.println("git is SCM");
+		System.out.println("git is powerful");
 		System.out.println("git is DVCS");
 
 	}
